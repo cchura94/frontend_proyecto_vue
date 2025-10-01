d9492011a91fff4f182a4544b5a6ab205891e5d8
cristian <cchura.cpc@gmail.com>
2025-09-30 22:21:16 -0400

    navegacion + estilos tailwindcss sitioweb

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,5 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-    <h1>App.vue</h1>
-
-    <nav>
-      <RouterLink to="/">INICIO</RouterLink> |
-      <RouterLink to="/nosotros">Acerca de Nosotros</RouterLink> |
-      <RouterLink to="/servicios">Nuestros Servicios</RouterLink>
-    </nav>
-
-    <RouterView />
-
+  <RouterView />
 </template>
 
+<script setup lang="ts"></script>
---
+++ src/layout/AppLayout.vue
@@ -0,0 +1,8 @@
+<template>
+    <nav>
+      <RouterLink to="/admin/perfil">Mi Perfil</RouterLink> |
+      <RouterLink to="/admin/usuario">Usuarios</RouterLink>
+    </nav>
+
+    <RouterView />
+</template>
