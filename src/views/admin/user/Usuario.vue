362971ed88f2a6d4cf2bbf03110f12d89eb21dfc
cristian <cchura.cpc@gmail.com>
2025-10-02 22:24:41 -0400

    interceptores Vue + Lista de Usuarios

--- src/views/admin/user/Usuario.vue
+++ src/views/admin/user/Usuario.vue
@@ -1,3 +1,28 @@
 <template>
     <h1>Usuarios</h1>
+    <pre>{{ usuarios }}</pre>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+import * as userService  from "./../../../services/user.service"
+// import algo, { guardarApiUsuario } from "./../../../services/user.service";
+
+const usuarios = ref([])
+
+onMounted(() => {
+    obtenerUsuarios();
+})
+
+async function obtenerUsuarios(){
+    try {
+        // const { data } = await userService.listarApiUsuario()
+        // usuarios.value = data;
+        const respuesta = await userService.listarApiUsuario()
+        usuarios.value = respuesta.data
+    } catch (error) {
+        console.log(error);
+    }
+}
+</script>
